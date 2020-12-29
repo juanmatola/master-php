9c71d505ff2e2f3093d42fb89120de5c5c24fe5c
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2020-12-29 15:02:41 -0300

    Sentencias basicas de sql

---
+++ aprendiendo-sql/crear-tabla.sql
@@ -0,0 +1,15 @@
+//SENTENCIAS EN SQL
+
+// Crear tabla
+CREATE TABLE usuarios(
+    id          int(11),
+    nombre      varchar(100),
+    appelido    varchar(100),
+    email       varchar(100),
+    password    varchar(100)
+);
+
+// mostrar tablas: show tables;
+
+//Eliminar tabla
+drop table usuarios;
